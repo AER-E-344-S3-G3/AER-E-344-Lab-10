1bd634f20e36faae8d51a8b2d9f17d5043fcad79
Matthew Mehrtens <matthew@mehrtens.cc>
2024-04-11 23:59:49 -0500

    Updated table printing

--- Code/table2latex.m
+++ Code/table2latex.m
@@ -1,36 +1,8 @@
-% ----------------------------------------------------------------------- %
-% Function table2latex(T, filename) converts a given MATLAB(R) table into %
-% a plain .tex file with LaTeX formatting.                                %
-%                                                                         %
-%   Input parameters:                                                     %
-%       - T:        MATLAB(R) table. The table should contain only the    %
-%                   following data types: numeric, boolean, char or string.
-%                   Avoid including structs or cells.                     %
-%       - filename: (Optional) Output path, including the name of the file.
-%                   If not specified, the table will be stored in a       %
-%                   './table.tex' file.                                   %  
-% ----------------------------------------------------------------------- %
-%   Example of use:                                                       %
-%       LastName = {'Sanchez';'Johnson';'Li';'Diaz';'Brown'};             %
-%       Age = [38;43;38;40;49];                                           %
-%       Smoker = logical([1;0;1;0;1]);                                    %
-%       Height = [71;69;64;67;64];                                        %
-%       Weight = [176;163;131;133;119];                                   %
-%       T = table(Age,Smoker,Height,Weight);                              %
-%       T.Properties.RowNames = LastName;                                 %
-%       table2latex(T);                                                   %                                       
-% ----------------------------------------------------------------------- %
-%   Version: 1.1                                                          %
-%   Author:  Victor Martinez Cagigal                                      %
-%   Date:    09/10/2018                                                   %
-%   E-mail:  vicmarcag (at) gmail (dot) com                               %
-% ----------------------------------------------------------------------- %
 function table2latex(T, filename, column_names)
-    
-    % Error detection and default parameters
     if nargin < 2
         filename = 'table.tex';
-        fprintf('Output path is not defined. The table will be written in %s.\n', filename); 
+        fprintf(['Output path is not defined. The table will be ' ...
+            'written in %s.\n'], filename);
     elseif ~ischar(filename)
         error('The output file name must be a string.');
     else
@@ -60,24 +32,23 @@ function table2latex(T, filename, column_names)
     fprintf(fileID, '\\midrule\n');
     
     % Writing the data
-    try
-        for row = 1:size(T,1)
-            temp{1,n_col} = [];
-            for col = 1:n_col
-                value = T{row,col};
-                if isstruct(value), error('Table must not contain structs.'); end
-                while iscell(value), value = value{1,1}; end
-                if isinf(value), value = '$\infty$'; end
-                temp{1,col} = num2str(value);
+    for row = 1:size(T,1)
+        temp{1,n_col} = [];
+        for col = 1:n_col
+            value = T{row,col};
+            if isstruct(value)
+                error('Table must not contain structs.');
             end
-            if ~isempty(row_names)
-                temp = [row_names{row}, temp];
-            end
-            fprintf(fileID, '%s \\\\ \n', strjoin(temp, ' & '));
-            clear temp;
+            while iscell(value), value = value{1,1}; end
+            if isinf(value), value = '$\infty$'; end
+            temp{1,col} = convertStringsToChars("\num{" ...
+                + num2str(value) + "}");
         end
-    catch
-        error('Unknown error. Make sure that table only contains chars, strings or numeric values.');
+        if ~isempty(row_names)
+            temp = [row_names{row}, temp];
+        end
+        fprintf(fileID, '%s \\\\ \n', strjoin(temp, ' & '));
+        clear temp;
     end
     
     % Closing the file
